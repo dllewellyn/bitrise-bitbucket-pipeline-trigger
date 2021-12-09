5d814f2c39cc6eb51fc70edce325daf3a79685bf
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2021-12-09 17:05:01 +0000

    Adding more step variables

--- step.sh
+++ step.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+echo $bitbucket_repo
+echo $branch 
+echo $bitbucket_slug
+echo $bitbucket_workspace
+
 curl https://api.bitbucket.org/2.0/repositories/xdesign365/marvin-android/pipelines/  -u ${bitbucket_username}:${bitbucket_app_password}  -H "content-type: application/json" -d '{"target": { "ref_type": "branch",  "type": "pipeline_ref_target", "ref_name": "${branch}", "selector": { "type":"custom", "pattern":"deployed" } }}' -X POST
 
 
