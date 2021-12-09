83dfd42882f042684d8e3827756dea2a2d02aa75
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2021-12-09 17:59:54 +0000

    :)

--- step.sh
+++ step.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-curl https://api.bitbucket.org/2.0/repositories/${bitbucket_workspace}/${bitbucket_slug}/pipelines/  -u ${bitbucket_username}:${bitbucket_app_password}  -H "content-type: application/json" -d '{"target": { "ref_type": "branch",  "type": "pipeline_ref_target", "ref_name": "$branch", "selector": { "type":"custom", "pattern":$pipeline_to_trigger" } }}' -X POST
+curl https://api.bitbucket.org/2.0/repositories/${bitbucket_workspace}/${bitbucket_slug}/pipelines/  -u ${bitbucket_username}:${bitbucket_app_password}  -H "content-type: application/json" -d '{"target": { "ref_type": "branch",  "type": "pipeline_ref_target", "ref_name": $branch" "selector": { "type":"custom", "pattern":$pipeline_to_trigger } }}' -X POST
