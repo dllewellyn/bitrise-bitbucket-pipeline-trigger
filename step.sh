5d2e1bc20d1a95cc53800023d5f76f4e43d9de42
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2021-12-09 17:05:59 +0000

    Adding steps

--- step.sh
+++ step.sh
@@ -5,8 +5,9 @@ echo $bitbucket_repo
 echo $branch 
 echo $bitbucket_slug
 echo $bitbucket_workspace
+echo $pipeline_to_trigger
 
-curl https://api.bitbucket.org/2.0/repositories/xdesign365/marvin-android/pipelines/  -u ${bitbucket_username}:${bitbucket_app_password}  -H "content-type: application/json" -d '{"target": { "ref_type": "branch",  "type": "pipeline_ref_target", "ref_name": "${branch}", "selector": { "type":"custom", "pattern":"deployed" } }}' -X POST
+curl https://api.bitbucket.org/2.0/repositories/xdesign365/marvin-android/pipelines/  -u ${bitbucket_username}:${bitbucket_app_password}  -H "content-type: application/json" -d '{"target": { "ref_type": "branch",  "type": "pipeline_ref_target", "ref_name": "${branch}", "selector": { "type":"custom", "pattern":"${pipeline_to_trigger}" } }}' -X POST
 
 
 #
