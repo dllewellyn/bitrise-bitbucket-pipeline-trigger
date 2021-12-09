849d880fb74c08ee28c8962c7842e25b26ebbfcb
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2021-12-09 16:22:53 +0000

    Adding variables

--- step.sh
+++ step.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+curl https://api.bitbucket.org/2.0/repositories/xdesign365/marvin-android/pipelines/  -u ${bitbucket_username}:${bitbucket_app_password}  -H "content-type: application/json" -d '{"target": { "ref_type": "branch",  "type": "pipeline_ref_target", "ref_name": "${branch}", "selector": { "type":"custom", "pattern":"deployed" } }}' -X POST
+
 
 #
 # --- Export Environment Variables for other Steps:
 # You can export Environment Variables for other Steps with
 #  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
 envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
+# A very simple example:
 # Envman can handle piped inputs, which is useful if the text you want to
 # share is complex and you don't want to deal with proper bash escaping:
 #  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
